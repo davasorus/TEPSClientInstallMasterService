d052cd2685d535533a325da88ef94e1680c04d6a
Sean Davitt <davitts2@gmail.com>
2022-10-08 19:59:23 -0400

    embedded file fixing, stored procedure fixing

--- Resources/Scripts/4.31 CreateSPUpdateCatalogSQLComp3564.sql
+++ Resources/Scripts/4.31 CreateSPUpdateCatalogSQLComp3564.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE [dbo].[UpdateCatalogSQLComp3564]
 	@Client_ID int,
-	@SQLCompact35364_installed bit
+	@SQLCompact3564_installed bit
 AS
 	update InstalledCatalog
 	SET SQLCompact3564_Installed = @SQLCompact3564_installed,

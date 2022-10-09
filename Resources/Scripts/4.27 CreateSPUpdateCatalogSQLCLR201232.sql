32710e80badfb3c744b4bc01432475b9bf3d8657
Sean Davitt <davitts2@gmail.com>
2022-10-09 12:12:40 -0400

    1.22.10.8 release

--- Resources/Scripts/4.27 CreateSPUpdateCatalogSQLCLR201232.sql
+++ Resources/Scripts/4.27 CreateSPUpdateCatalogSQLCLR201232.sql
@@ -1,8 +1,8 @@
 CREATE PROCEDURE [dbo].[UpdateCatalogSQLCLR201232]
 	@Client_ID int,
-	@SQLCLR201264_installed bit
+	@SQLCLR201232_installed bit
 AS
 	update InstalledCatalog
-	SET [SQLCLR201264_Installed] = @SQLCLR201264_installed,
+	SET [SQLCLR201232_Installed] = @SQLCLR201232_installed,
 	ModifiedDate_time = (GETDATE())
 	where Client_ID = @Client_ID
